2a3ba2358defbe80bd3b273ed3f26c1d7ac6bfe2
anna bateman <annacbateman@gmail.com>
2020-04-07 22:01:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -12,11 +12,11 @@ def find_element_index(array, value_to_find)
   # end
   # found_value_index
   
-  count = 0
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1
-  end
+  # count = 0
+  # while count < array.length do
+  #   if array[count] == value_to_find
+  #     return count
+  #   end
+  #   count += 1
+  # end
 end
